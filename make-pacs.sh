917e1d373d2bb29305bddafedf8a9188ab40f502
Aurorans Solis <primalucegd@gmail.com>
2022-04-03 14:19:05 -0700

    Made it possible to build specific PACs.

--- make-pacs.sh
+++ make-pacs.sh
@@ -14,18 +14,15 @@ then
     return
 fi
 
-set -xe
-
-for svd in svd/*.svd
-do
-    chip_upper=$(basename "${svd}" ".svd")
+function build_pac() {
+    chip_upper=$(basename "${1}" ".svd")
     chip_lower=$(echo "${chip_upper}" | tr '[:upper:]' '[:lower:]')
     xsl=svd/patches/${chip_lower}.xsl
     pac_dir=pac/${chip_lower}
 
     mkdir -p ${pac_dir}
 
-    xsltproc ${xsl} ${svd} \
+    xsltproc ${xsl} ${1} \
         | svd2rust --nightly --const_generic --target cortex-m --output-dir ${pac_dir}
     form -i ${pac_dir}/lib.rs -o ${pac_dir}/src/
     rm ${pac_dir}/lib.rs
@@ -34,4 +31,29 @@ do
     cargo +nightly fmt
     rustfmt +nightly build.rs
     popd
-done
+}
+
+if [[ -z "$@" ]]
+then
+    echo "No target device(s) provided. Building all PACs."
+    for svd in svd/*.svd
+    do
+        set -xe
+        build_pac "$svd"
+        set +xe
+    done
+else
+    echo "Targets provided: '$@'"
+    for target_arg in "$@"
+    do
+        if [ -f "svd/$1.svd" ]
+        then
+            echo "Building PAC for device: $1"
+            set -xe
+            build_pac "svd/$1.svd"
+            set +xe
+        else
+            echo "Unknown device: '$1'"
+        fi
+    done
+fi
