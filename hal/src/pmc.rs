1a1a8292c887b22b2d1c347229e4704a0ea87344
Aurorans Solis <primalucegd@gmail.com>
2024-04-18 15:55:41 -0700

    Missed this in an earlier commit with svd2rust changes.

--- hal/src/pmc.rs
+++ hal/src/pmc.rs
@@ -1,24 +1,24 @@
-use crate::pac::{pmc::pmc_mckr::PRES_A, PMC};
+use crate::pac::{pmc::pmc_mckr::Pres, PMC};
 
-pub fn set_master_clk_prescaler(pmc: &PMC, prescaler: PRES_A) {
+pub fn set_master_clk_prescaler(pmc: &PMC, prescaler: Pres) {
     // Set prescaler bits
-    pmc.pmc_mckr
+    pmc.pmc_mckr()
         .write(|master_clk_reg| master_clk_reg.pres().variant(prescaler));
     // Wait for clock to be ready again
-    while pmc.pmc_sr.read().mckrdy().bit_is_clear() {}
+    while pmc.pmc_sr().read().mckrdy().bit_is_clear() {}
 }
 
 // Only defined for peripheral IDs 9, 11-44
 pub(crate) unsafe fn enable_peripheral_clk(pmc: &PMC, id: u32) {
     if id < 32 {
         let mask = 1 << id;
-        if pmc.pmc_pcsr0.read().bits() & mask == 0 {
-            pmc.pmc_pcer0.write_with_zero(|reg| reg.bits(mask));
+        if pmc.pmc_pcsr0().read().bits() & mask == 0 {
+            pmc.pmc_pcer0().write_with_zero(|reg| reg.bits(mask));
         }
     } else {
         let mask = 1 << (id - 32);
-        if pmc.pmc_pcsr1.read().bits() & mask == 0 {
-            pmc.pmc_pcer1.write_with_zero(|reg| reg.bits(mask));
+        if pmc.pmc_pcsr1().read().bits() & mask == 0 {
+            pmc.pmc_pcer1().write_with_zero(|reg| reg.bits(mask));
         }
     }
 }
@@ -26,13 +26,13 @@ pub(crate) unsafe fn enable_peripheral_clk(pmc: &PMC, id: u32) {
 pub(crate) unsafe fn disable_peripheral_clk(pmc: &PMC, id: u32) {
     if id < 32 {
         let mask = 1 << id;
-        if pmc.pmc_pcsr0.read().bits() & mask == mask {
-            pmc.pmc_pcdr0.write_with_zero(|reg| reg.bits(mask));
+        if pmc.pmc_pcsr0().read().bits() & mask == mask {
+            pmc.pmc_pcdr0().write_with_zero(|reg| reg.bits(mask));
         }
     } else {
         let mask = 1 << (id - 32);
-        if pmc.pmc_pcsr1.read().bits() & mask == mask {
-            pmc.pmc_pcdr1.write_with_zero(|reg| reg.bits(mask));
+        if pmc.pmc_pcsr1().read().bits() & mask == mask {
+            pmc.pmc_pcdr1().write_with_zero(|reg| reg.bits(mask));
         }
     }
 }
